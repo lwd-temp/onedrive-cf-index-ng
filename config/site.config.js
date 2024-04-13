8c681f66b79f4e3ca5ba0fdd726da4d9aed5d1f2
Sunset Mikoto <26019675+lwd-temp@users.noreply.github.com>
2024-04-13 16:49:58 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -5,19 +5,22 @@
 module.exports = {
   // This is what we use to identify who you are when you are initialising the website for the first time.
   // Make sure this is exactly the same as the email address you use to sign into your Microsoft account.
-  // You can also put this in your worker's environment variable if you worry about
+  // You can also put this in your Vercel's environment variable 'NEXT_PUBLIC_USER_PRINCIPLE_NAME' if you worry about
   // your email being exposed in public.
-  userPrincipalName: process.env.USER_PRINCIPLE_NAME || 'me@lyc8503.site',
+  userPrincipalName: process.env.NEXT_PUBLIC_USER_PRINCIPLE_NAME || 'drive@sunsetmikoto.onmicrosoft.com',
 
   // [OPTIONAL] This is the website icon to the left of the title inside the navigation bar. It should be placed under the
   // /public directory of your GitHub project (not your OneDrive folder!), and referenced here by its relative path to /public.
   icon: '/icons/128.png',
 
-  // The name of your website. Present alongside your icon.
-  title: "lyc8503's OneDrive",
+  // Prefix for KV Storage
+  kvPrefix: process.env.KV_PREFIX || '',
 
-  // The folder that you are to share publicly with onedrive-cf-index-ng. Use '/' if you want to share your root folder.
-  baseDirectory: '/',
+  // The name of your website. Present alongside your icon.
+  title: "Sunset's OneDrive",
+
+  // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
+  baseDirectory: '/Public',
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
@@ -25,9 +28,9 @@ module.exports = {
 
   // [OPTIONAL] We use Google Fonts natively for font customisations.
   // You can check and generate the required links and names at https://fonts.google.com.
-  // googleFontSans - the sans serif font used in onedrive-cf-index-ng.
+  // googleFontSans - the sans serif font used in onedrive-vercel-index.
   googleFontSans: 'Inter',
-  // googleFontMono - the monospace font used in onedrive-cf-index-ng.
+  // googleFontMono - the monospace font used in onedrive-vercel-index.
   googleFontMono: 'Fira Mono',
   // googleFontLinks -  an array of links for referencing the google font assets.
   googleFontLinks: ['https://fonts.googleapis.com/css2?family=Fira+Mono&family=Inter:wght@400;500;700&display=swap'],
@@ -39,10 +42,10 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/Private', '/Demo/😎Another Private Folder Password 123'],
+  protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route', '/Secret'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  email: 'mailto:me@lyc8503.site',
+  email: 'mailto:i@lwd-temp.top',
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
@@ -50,7 +53,7 @@ module.exports = {
   links: [
     {
       name: 'GitHub',
-      link: 'https://github.com/lyc8503',
+      link: 'https://github.com/lyc8503/onedrive-cf-index-ng',
     },
   ],
 
